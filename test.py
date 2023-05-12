3bcd5e46748a91d100ac39c8217897e97653b412
Nefibata <119802671+Nefibata@users.noreply.github.com>
2023-05-12 21:56:50 +0800

    Update test.py

--- test.py
+++ test.py
@@ -12,6 +12,6 @@ def get_host_ip(): # 查询本机ip并且返回ip
 # 这是你需要根据自己的情况修改的地方
 
 
-url = 'http://10.160.63.9:801/eportal/?c=Portal&a=login&callback=dr1683867797076&login_method=1&user_account='+'你的账号'+'@'账号的网络类型'+'@cmcc&user_password='+'账号密码'+'&wlan_user_ip='+get_host_ip()+'&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.0&_=1683852619821'
+url = 'http://10.160.63.9:801/eportal/?c=Portal&a=login&callback=dr1683867797076&login_method=1&user_account='+'你的账号'+'@'+'账号的网络类型'+'@cmcc&user_password='+'账号密码'+'&wlan_user_ip='+get_host_ip()+'&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.0&_=1683852619821'
 response = requests.get(url).status_code  # 直接利用 GET 方式请求这个 URL 同时获取状态码
 print("状态码{}".format(response))  # 打印状态码
